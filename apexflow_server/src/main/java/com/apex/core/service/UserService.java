4cd47b1e44837d5ccef96dea20b9e7d63fdb3d2e
yurilt_s <33693@qq.com>
2025-12-30 18:02:27 +0800

    修复警告

--- apexflow_server/src/main/java/com/apex/core/service/UserService.java
+++ apexflow_server/src/main/java/com/apex/core/service/UserService.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 public class UserService {
 
-    private IUserDAO userDAO;
+    private final IUserDAO userDAO;
 
     public UserService() {
         this.userDAO = new UserDAO();
